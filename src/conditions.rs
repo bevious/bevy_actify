021739bf884895d41455eaad535e83c72904c7f0
iizudev <izudev@yandex.ru>
2025-03-26 10:20:51 +0500

    add status conditions

--- src/conditions.rs
+++ src/conditions.rs
@@ -1,6 +1,30 @@
-use crate::{InputAction, InputActionState};
+use crate::{InputAction, InputActionReader, InputActionState, InputActionStatus};
 
 /// Returns `true` if the input action [`A`] is active.
 pub fn input_action_active<A: InputAction>(action: InputActionState<A>) -> bool {
     action.is_active()
+}
+
+/// Returns `true` if the input action [`A`] has just started.
+pub fn input_action_started<A: InputAction>(mut action: InputActionReader<A>) -> bool {
+    action
+        .read()
+        .last()
+        .map_or(false, |a| matches!(a, InputActionStatus::Started(_)))
+}
+
+/// Returns `true` if the input action [`A`] has just updated.
+pub fn input_action_updated<A: InputAction>(mut action: InputActionReader<A>) -> bool {
+    action
+        .read()
+        .last()
+        .map_or(false, |a| matches!(a, InputActionStatus::Updated(_)))
+}
+
+/// Returns `true` if the input action [`A`] has just stopped.
+pub fn input_action_stopped<A: InputAction>(mut action: InputActionReader<A>) -> bool {
+    action
+        .read()
+        .last()
+        .map_or(false, |a| matches!(a, InputActionStatus::Stopped))
 }
